a5c4780396a5c26817f920ee2540a1bef35eae42
Seydi Charyyev <seydi.charyev@gmail.com>
2025-05-26 11:00:37 +0500

    Update ExperienceSection.jsx

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -37,8 +37,8 @@ const ExperienceSection = () => {
                                                 <p className='text-[#839cb5 italic]'>
                                                     Responsibilities
                                                 </p>
-                                                <ul className='list-disc ms-5'>
-
+                                                <ul className='list-disc ms-5 mt-5 flex flex-col gap-5 text-white-50'>
+                                                    {card.responsibilities.map((responsibility))}
                                                 </ul>
                                             </div>
                                         </div>

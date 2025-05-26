23f532b95531fad59e48f1af77d056c59cec72fd
Seydi Charyyev <seydi.charyev@gmail.com>
2025-05-26 10:56:54 +0500

    Update ExperienceSection.jsx

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -30,7 +30,11 @@ const ExperienceSection = () => {
                                                 <img src={card.logoPath} alt='logo' />
                                             </div>
                                             <div>
-                                                <h1>{card.title}</h1>
+                                                <h1 className='font-semibold text-3xl'>{card.title}</h1>
+                                                <p className='my-5 text-white-50'>
+                                                    {card.date}
+
+                                                </p>
                                             </div>
 
                                         </div>

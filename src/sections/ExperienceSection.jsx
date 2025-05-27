b23466341a66a6c386cb458d0b1bf728ff09f5dd
Seydi Charyyev <seydi.charyev@gmail.com>
2025-05-27 16:47:00 +0500

    Update ExperienceSection.jsx

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -5,8 +5,21 @@ import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
+gsap.registerPlugin(ScrollTrigger);
+
 
 const ExperienceSection = () => {
+
+    useGSAP(() => {
+
+        gsap.utils.toArray('.time-card').forEach((card) => {
+            gsap.from(card)
+        })
+
+
+    }, []);
+
+
     return (
         <section id="experience" className="w-full md:mt-40 mt-20 section-padding xl:px-0">
             <div className='w-full h-full md:px-20 px-5'>

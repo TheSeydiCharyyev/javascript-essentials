290709fed6af25a370abe24aaec31f340d753ded
Seydi Charyyev <seydi.charyev@gmail.com>
2025-05-29 17:58:43 +0500

    Update ExperienceSection.jsx

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -5,6 +5,7 @@ const ExperienceSection = () => {
     return (
         <section id='experience' classname='w-full md:mt-40 mt-20 section-padding xl:px-0'>
             <div className='w-full h-full md:px-20 px-5'>
+
                 <TitleHeader title='Professional Work Experience'
                     sub='My Career Overview' />
 

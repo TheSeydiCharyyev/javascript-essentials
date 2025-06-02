3ec781e086d879296a2656ad1615051d58b9c6d0
Seydi Charyyev <seydi.charyev@gmail.com>
2025-06-02 17:02:34 +0500

    Update TechIcon.jsx

--- src/components/Models/TechLogos/TechIcon.jsx
+++ src/components/Models/TechLogos/TechIcon.jsx
@@ -1,11 +1,15 @@
-import { useGLTF } from "@react-three/drei"
+import { Environment, useGLTF } from "@react-three/drei"
+import { Canvas } from "@react-three/fiber";
 
 const TechIcon = ({ model }) => {
     const scene = useGLTF(model.modelPath);
 
     return (
         <div>
-
+            <Canvas>
+                <ambientLight intensity={0.3} />
+                <Environment preset="city" />
+            </Canvas>
         </div>
     )
 }

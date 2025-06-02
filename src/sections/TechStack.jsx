f9f73b53d35f699a93db65180fd231e39cd88ca2
Seydi Charyyev <seydi.charyev@gmail.com>
2025-06-02 16:55:47 +0500

    Update TechStack.jsx

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -1,3 +1,4 @@
+import TechIcon from '../components/Models/TechLogos/TechIcon'
 import TitleHeader from '../components/TitleHeader'
 import { techStackIcons } from '../constants'
 
@@ -12,7 +13,15 @@ const TechStack = () => {
                 <div className='tech-grid'>
                     {techStackIcons.map((icon) => (
                         <div key={icon.name} className='card-border tech-card overflow-hidden group xl:rounded-full rounded-lg'>
-                            {icon.name}
+                            <div className='tech-card-animated-bg' />
+                            <div className='tech-card-content'>
+                                <div className='tech-icon-wrapper'>
+                                    <TechIcon model={icon} />
+                                </div>
+                                <div>
+
+                                </div>
+                            </div>
                         </div>
                     ))}
                 </div>

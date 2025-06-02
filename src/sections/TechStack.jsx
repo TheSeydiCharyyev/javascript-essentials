79ecc11de725055d79d35a02647ecf73ebd0b4f8
Seydi Charyyev <seydi.charyev@gmail.com>
2025-06-02 16:47:48 +0500

    Update TechStack.jsx

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -12,7 +12,7 @@ const TechStack = () => {
                 <div className='tech-grid'>
                     {techStackIcons.map((icon) => (
                         <div key={icon.name}>
-
+                            {icon.name}
                         </div>
                     ))}
                 </div>

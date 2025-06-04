44fa108dfd78d2bc121667e67adc027adeae956c
Seydi Charyyev <seydi.charyev@gmail.com>
2025-06-04 09:54:45 +0500

    Update TechStack.jsx

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -44,7 +44,13 @@ const TechStack = () => {
                         <div key={icon.name} className='card-border tech-card overflow-hidden group xl:rounded-full rounded-lg'>
                             <div className='tech-card-animated-bg' />
                             <div className='tech-card-content'>
-                                <div></div>
+                                <div className='tech-icon-wrapper'>
+                                    <img src={icon.imgPath} />
+                                </div>
+                                <div className='padding-x w-full'>
+                                    <p>{icon.name}</p>
+
+                                </div>
                             </div>
                         </div>
                     ))}

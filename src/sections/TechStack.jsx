ac56025bb5792c96f1272e14038f99afb208453f
Seydi Charyyev <seydi.charyev@gmail.com>
2025-06-04 09:42:26 +0500

    Update TechStack.jsx

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -1,8 +1,15 @@
+import { useGSAP } from '@gsap/react'
 import TechIcon from '../components/Models/TechLogos/TechIcon'
 import TitleHeader from '../components/TitleHeader'
 import { techStackIcons } from '../constants'
+import { gsap } from 'gsap'
 
 const TechStack = () => {
+    useGSAP(() => {
+        gsap.fromTo
+    })
+
+
     return (
         <div id='skills' className='flex-center section-padding'>
             <div className='w-full h-full md:px-10 px-5'>

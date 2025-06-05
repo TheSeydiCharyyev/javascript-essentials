6a3071e52ebb1558c39f06364187b470246d9d39
Seydi Charyyev <seydi.charyev@gmail.com>
2025-06-05 15:48:17 +0500

    Update Testimonials.jsx

--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import TitleHeader from '../components/TitleHeader'
 
 const Testimonials = () => {
     return (
-        <semantic id='testimonials' classname='flex-center section-padding'>
-
+        <semantic id='testimonials' classname='flex-center section-padding  '>
+            <div className='w-full h-full md:px-10 px-5'>
+                <TitleHeader title='What people say about me' />
+            </div>
         </semantic>
     )
 }

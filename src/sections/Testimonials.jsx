7fca28b8c95ab6a0b586b56543830ea0f6cbeec3
Seydi Charyyev <seydi.charyev@gmail.com>
2025-06-05 16:05:24 +0500

    Update Testimonials.jsx

--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -17,9 +17,8 @@ const Testimonials = () => {
                                     <img src={imgPath} alt={name} />
                                 </div>
                                 <div>
-                                    <p className='font-bold'>
-                                        {name}
-                                    </p>
+                                    <p className='font-bold'>{name}</p>
+                                    <p className='text-white-50'>{mentions}</p>
                                 </div>
                             </div>
                         </GlowCard>

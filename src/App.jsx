fae8de4bbe9b7922b15f1a0cd30f2c0ce0fefb2a
Seydi Charyyev <seydi.charyev@gmail.com>
2025-06-05 15:46:42 +0500

    Testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import ShowcaseSection from "./sections/ShowcaseSection"
 import FeatureCards from "./sections/FeatureCards";
 import ExperienceSection from "./sections/ExperienceSection";
 import TechStack from "./sections/TechStack";
+import Testimonials from "./sections/Testimonials";
 
 const app = () => {
     return (
@@ -16,6 +17,7 @@ const app = () => {
             <FeatureCards />
             <ExperienceSection />
             <TechStack />
+            <Testimonials />
 
         </>
 
---
+++ src/sections/Testimonials.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Testimonials = () => {
+    return (
+        <semantic id='testimonials' classname='flex-center section-padding'>
+
+        </semantic>
+    )
+}
+
+export default Testimonials

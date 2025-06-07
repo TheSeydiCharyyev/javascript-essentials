804b3fc1d8f3bfdee41ee32c6f9f4463f1e68a9e
Seydi Charyyev <seydi.charyev@gmail.com>
2025-06-07 16:15:42 +0500

    Update GlowCard.jsx

--- src/components/GlowCard.jsx
+++ src/components/GlowCard.jsx
@@ -1,47 +1,49 @@
-import React, { useRef } from "react";
+import { useRef } from "react";
 
-const GlowCard = ({ card, children, index }) => {
-
+const GlowCard = ({ card, index, children }) => {
+    // refs for all the cards
     const cardRefs = useRef([]);
 
+    // when mouse moves over a card, rotate the glow effect
     const handleMouseMove = (index) => (e) => {
+        // get the current card
         const card = cardRefs.current[index];
         if (!card) return;
 
-        // Get the mouse position relative to card
+        // get the mouse position relative to the card
         const rect = card.getBoundingClientRect();
         const mouseX = e.clientX - rect.left - rect.width / 2;
         const mouseY = e.clientY - rect.top - rect.height / 2;
 
-        // calc the angle from the center of the card
-
+        // calculate the angle from the center of the card to the mouse
         let angle = Math.atan2(mouseY, mouseX) * (180 / Math.PI);
+
+        // adjust the angle so that it's between 0 and 360
         angle = (angle + 360) % 360;
 
-        card.style.setProperty('--start', angle + 60)
-    }
+        // set the angle as a CSS variable
+        card.style.setProperty("--start", angle + 60);
+    };
 
+    // return the card component with the mouse move event
     return (
-        <div ref={(el) => (cardRefs.current[index] = el)} onMouseMove={handleMouseMove(index)} className='card car-border time-card rounded-xl p-10 mb-5 break-inside-avoid-column'>
-
-            <div className='glow' />
-            <div className='flex items-center gap-1 mb-5'>
+        <div
+            ref={(el) => (cardRefs.current[index] = el)}
+            onMouseMove={handleMouseMove(index)}
+            className="card card-border timeline-card rounded-xl p-10 mb-5 break-inside-avoid-column"
+        >
+            <div className="glow"></div>
+            <div className="flex items-center gap-1 mb-5">
                 {Array.from({ length: 5 }, (_, i) => (
-                    <img src='/images/star.png' key={i} alt="star" className='size-5' />
+                    <img key={i} src="/images/star.png" alt="star" className="size-5" />
                 ))}
             </div>
-            <div className='mb-5'>
-                <p className='text-white-50 text-lg'>
-                    {card.review}
-                </p>
-
+            <div className="mb-5">
+                <p className="text-white-50 text-lg">{card.review}</p>
             </div>
             {children}
         </div>
-    )
-}
-{/*
-    End of Testimonials section
-    */}
+    );
+};
 
-export default GlowCard
+export default GlowCard;
